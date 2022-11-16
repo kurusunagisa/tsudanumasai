47762e20546f3d42162f5a0207a9556a7faf7f07
Sakaguchi Tsubasa <17411@g.nagano-nct.ac.jp>
2022-11-17 02:49:05 +0900

    20221116

--- slave/index.js
+++ slave/index.js
@@ -12,13 +12,62 @@ var player = new Player({
     accessToken: '00000101-b8RiuAV' // アクセストークン
 });
 player.addPlugin(new Plugin.SongleSync());
-player.addPlugin(new Plugin.Beat({offset: -100}));
+player.addPlugin(new Plugin.Beat({offset: 20}));
+player.addPlugin(new Plugin.Variation({groupCount: 3, offset: 630}));
+player.addPlugin(new Plugin.Chorus({offset: 630}));
+
+let light = "88";
 
 player.on("beatPlay",
     function(ev) {
-        write((ev.data.beat.number).toString());
+        if(ev.data.beat.number == 1){
+            //flash
+            //write("11");
+            //round
+            write(light);
+        }else{
+            write("88");
+        }
     }
 )
+
+player.on("variationEnter",
+    function(ev){
+        if(light != 14){
+            switch(ev.data.variation.group){
+                case 0:
+                    light = "24";
+                    break;
+                case 1:
+                    light = "34";
+                    break;
+                case 2:
+                    light = "14";
+                    break;
+                default:
+                    light = "77";
+            }
+        }
+    }
+);
+
+player.on("chorusSectionEnter",
+    function(ev){
+        light = "14";
+    }
+);
+
+player.on("chorusSectionLeave",
+    function(ev){
+        light = "34";
+    }
+);
+
+player.on("pause",
+    function(){
+        write("99");
+    }
+);
 
 
 function write(data) {
@@ -29,4 +78,4 @@ function write(data) {
             console.log('Results: ' + results);
         }
     });
-  }
+}
