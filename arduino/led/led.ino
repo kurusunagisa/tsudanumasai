f20caa3e4934f4f0bf0f0c035e73314f2761f6b8
Sakaguchi Tsubasa <17411@g.nagano-nct.ac.jp>
2022-11-17 02:49:12 +0900

    20221116

--- arduino/led/led.ino
+++ arduino/led/led.ino
@@ -9,8 +9,14 @@
 #define LED_TYPE WS2812B
 #define COLOR_ORDER RGB
 
-#define NUM_LEDS_8 8
-#define RING_PIN_8 9
+#define NUM_LEDS_16 16
+#define RING_PIN_16 9
+
+#define NUM_LEDS_35 35
+#define RING_PIN_35 8
+
+#define NUM_LEDS_45 45
+#define RING_PIN_45 7
 
 int recieveByte = 0;
 String bufferStr = "";
@@ -30,6 +36,9 @@ bool reverse = false;
 bool ledEnd = false;
 int ledCount = 0;
 float diffTime = 0;
+
+bool roundFlag = false;
+bool alterFlag = false;
 
 void updateVolumeLED(){
   float currentTime = micros();
@@ -54,21 +63,289 @@ void updateVolumeLED(){
   }
 }
 
-CRGB ring8_leds[NUM_LEDS_8];
+//Ringライトの種類と光らせ方を選択する
+void selectLightingType(int lightingType, int ringNumber){
+  switch(lightingType){
+    case 1:
+      FastLED.setBrightness(90);
+      alterFlag = false;
+      setRingFlash(ringNumber);
+      break;
+    case 2:
+      FastLED.setBrightness(60);
+      alterFlag = false;
+      setRingRound(ringNumber);
+      break;
+    case 3:
+      FastLED.setBrightness(40);
+      alterFlag = true;
+      setRingAlternated(ringNumber);
+      break;
+  }
+}
+
+//フラッシュさせる
+void setRingFlash(int ringNumber){
+  switch(ringNumber){
+    case 1:
+      RingFlash16(true);
+      break;
+    case 2:
+      RingFlash35(true);
+      break;
+    case 3:
+      RingFlash45(true);
+    case 4:
+      RingFlash16(true);
+      RingFlash35(true);
+      RingFlash45(true);
+  }
+}
+
+void setRingRound(int ringNumber){
+  switch(ringNumber){
+    case 1:
+      RingRound16(true);
+      break;
+    case 2:  
+      RingRound35(true);
+      break;
+    case 3:
+      RingRound45(true);
+      break;
+    case 4:
+      RingRound16(true);
+      RingRound35(true);
+      RingRound45(true);
+  }
+}
+
+void setRingAlternated(int ringNumber){
+  switch(ringNumber){
+    case 1:
+      RingAlternated16(true);
+      break;
+    case 2:  
+      RingAlternated35(true);
+      break;
+    case 3:
+      RingAlternated45(true);
+      break;
+    case 4:
+      RingAlternated16(true);
+      RingAlternated35(true);
+      RingAlternated45(true);
+  }
+}
+
+//変化させるLED
+CRGB ring16_leds[NUM_LEDS_16];
+CRGB ring35_leds[NUM_LEDS_35];
+CRGB ring45_leds[NUM_LEDS_45];
+//インデックス
+int ring16Index = 0;
+int ring35Index = 0;
+int ring45Index = 0;
+
+int ring16Time = 0;
+int ring35Time = 0;
+int ring45Time = 0;
+
+
+//実時間ベースで変化させる
+/*void RingFlash8(bool flag){
+  if(flag){
+    ring8Index = 0;
+    FastLED.clear();
+  }
+  if(ring8Index < NUM_LEDS_8){  
+    ring8_leds[ring8Index] = CHSV(random(100,255),255,255);
+    FastLED.show();
+    FastLED.delay(2);
+    ring8Index++;
+  }
+}*/
+
+long r_16 = 0;
+long r_35 = 0;
+long r_45 = 0;
+
+void RingFlash16(bool flag){
+  if(flag){
+    r_16 = random(0,255);
+  }
+  for(int i = 0;i < NUM_LEDS_16;i++){
+    ring16_leds[i] = CHSV(r_16,255,255);
+  }
+  FastLED.show();
+}
+
+void RingFlash35(bool flag){
+  if(flag){
+    r_35 = random(0,255);
+  }
+  for(int i = 0;i < NUM_LEDS_35;i++){
+    ring35_leds[i] = CHSV(r_35,255,255);
+  }
+  FastLED.show();
+}
+
+void RingFlash45(bool flag){
+  if(flag){
+    r_45 = random(0,255);
+  }
+  for(int i = 0;i < NUM_LEDS_45;i++){
+    ring45_leds[i] = CHSV(r_45,255,255);
+  }
+  FastLED.show();
+}
+
+
+void RingRound16(bool flag){
+  float currentTime = micros();
+  if(flag){
+    ring16Index = 0;
+    FastLED.clear();
+    r_16 = random(0,255);
+    roundFlag = true;
+  }
+  if(ring16Index < NUM_LEDS_16 && currentTime - ring16Time > 3000000){  
+    ring16_leds[ring16Index] = CHSV(r_16,255,255);
+    FastLED.show();
+    ring16_leds[ring16Index] = CHSV(0,0,0);
+    ring16Index++;
+    ring16Time = currentTime;
+  }
+}
+
+void RingRound35(bool flag){
+  float currentTime = micros();
+  if(flag){
+    ring35Index = 0;
+    FastLED.clear();
+    r_35 = random(0,255);
+    roundFlag = true;
+  }
+  if(ring35Index < NUM_LEDS_35 && currentTime - ring35Time > 13){  
+    ring35_leds[ring35Index] = CHSV(r_35,255,255);
+    FastLED.show();
+    ring35_leds[ring35Index] = CHSV(0,0,0);
+    ring35Index++;
+    ring35Time = currentTime;
+  }
+}
+
+void RingRound45(bool flag){
+  float currentTime = micros();
+  if(flag){
+    ring45Index = 0;
+    FastLED.clear();
+    r_45 = random(0,255);
+    roundFlag = true;
+  }
+  if(ring45Index < NUM_LEDS_45 && currentTime - ring45Time > 7){  
+    ring45_leds[ring45Index] = CHSV(r_45,255,255);
+    FastLED.show();
+    ring45_leds[ring45Index] = CHSV(0,0,0);
+    ring45Index++;
+    ring45Time = currentTime;
+  }
+  if(ring45Index == NUM_LEDS_45){
+    roundFlag = false;
+  }
+}
+
+void RingAlternated16(bool flag){
+  float currentTime = micros();
+  if(flag){
+    ring16Index = 0;
+    FastLED.clear();
+    r_16 = random(0,255);
+  }
+  if(currentTime - ring16Time > 30){  
+    for(int i = 0; i < NUM_LEDS_16;i++){
+      ring16_leds[i] = CHSV((i+ring16Index)%2*r_16,(i+ring16Index)%2*255,(i+ring16Index)%2*255);
+    }
+    FastLED.show();
+    ring16Index++;
+    ring16Time = currentTime;
+  }
+}
+
+void RingAlternated35(bool flag){
+  float currentTime = micros();
+  if(flag){
+    ring35Index = 0;
+    FastLED.clear();
+    r_35 = random(0,255);
+  }
+  if(currentTime - ring35Time > 7){  
+    for(int i = 0; i < NUM_LEDS_35;i++){
+      ring35_leds[i] = CHSV((i+ring35Index)%2*r_35,(i+ring35Index)%2*255,(i+ring35Index)%2*255);
+    }
+    FastLED.show();
+    ring35Index++;
+    ring35Time = currentTime;
+  }
+}
+
+void RingAlternated45(bool flag){
+  float currentTime = micros();
+  if(flag){
+    ring45Index = 0;
+    FastLED.clear();
+    r_45 = random(0,255);
+  }
+  if(currentTime - ring35Time > 7){  
+    for(int i = 0; i < NUM_LEDS_45;i++){
+      ring45_leds[i] = CHSV((i+ring45Index)%2*r_45,(i+ring45Index)%2*255,(i+ring45Index)%2*255);
+    }
+    FastLED.show();
+    ring45Index++;
+    ring45Time = currentTime;
+  }
+}
+
+
+//何番目のテンポかを表示する
+void setTempo(char str){
+  int rb = str - '0';
+  digitalWrite(PIN_LATCH, LOW);
+  shiftOut(PIN_SER, PIN_CLK, LSBFIRST, (int(pow(2,rb)+1)>>1));
+  digitalWrite(PIN_LATCH, HIGH);
+}
+
+void clearAllLED(){
+  FastLED.clear();
+  for(int i = 0;i < NUM_LEDS_16;i++){
+    ring16_leds[i] = CHSV(0,0,0);
+  }
+  for(int i = 0;i < NUM_LEDS_35;i++){
+    ring35_leds[i] = CHSV(0,0,0);
+  }
+  for(int i = 0;i < NUM_LEDS_45;i++){
+    ring45_leds[i] = CHSV(0,0,0);
+  }
+  FastLED.show();
+}
+
 
 void setup() {
   pinMode(PIN_SER, OUTPUT);
   pinMode(PIN_LATCH, OUTPUT);
   pinMode(PIN_CLK, OUTPUT);
 
-  FastLED.addLeds<LED_TYPE, RING_PIN_8, COLOR_ORDER>(ring8_leds, NUM_LEDS_8).setCorrection(TypicalLEDStrip);
+  FastLED.addLeds<LED_TYPE, RING_PIN_16, COLOR_ORDER>(ring16_leds, NUM_LEDS_16).setCorrection(TypicalLEDStrip);
+  FastLED.addLeds<LED_TYPE, RING_PIN_35, COLOR_ORDER>(ring35_leds, NUM_LEDS_35).setCorrection(TypicalLEDStrip);
+  FastLED.addLeds<LED_TYPE, RING_PIN_45, COLOR_ORDER>(ring45_leds, NUM_LEDS_45).setCorrection(TypicalLEDStrip);
   FastLED.setBrightness(BRIGHTNESS);
 
   pinMode(LED, OUTPUT);
+  pinMode(RING_PIN_16, OUTPUT);
+  pinMode(RING_PIN_35, OUTPUT);
+  pinMode(RING_PIN_45, OUTPUT);
   Serial.begin(115200);
 }
-
-byte b = B11111000;
 
 void loop() {
   bufferStr = "";
@@ -78,31 +355,32 @@ void loop() {
     bufferStr.concat((char)recieveByte);
   }
   if(bufferStr.length() > 0){
-    digitalWrite(LED, HIGH);
-    //LEDバーの起動
-    ledEnd = false;
-    ledCount = 0;
-    //LEDリングの起動
-  }else{
-    digitalWrite(LED, LOW);
+    //digitalWrite(LED, HIGH);
+    String finish = "99";
+    if(finish.compareTo(bufferStr) == 0){
+      digitalWrite(LED,HIGH);
+      clearAllLED();
+    }else{
+      digitalWrite(LED,LOW);
+      //LEDバーの起動
+      ledEnd = false;
+      ledCount = 0;
+      //LEDリングの起動
+      selectLightingType(bufferStr[0] - '0',bufferStr[1] - '0');
+      if(alterFlag){
+        RingAlternated16(false);
+        RingAlternated35(false);
+        RingAlternated45(false);
+      }
+    }
+  }
+  //RingFlash35(false);
+  //RingFlash45(false);
+  if(roundFlag){
+    RingRound16(false);
+    RingRound35(false);
+    RingRound45(false);
   }
   updateVolumeLED();
-  delay(10);
-}
-
-//Ringライトの種類と光らせ方を選択する
-void setRingLED(int ringNumber, int lightingType){
-  switch(lightingType){
-    case 1:
-      
-      break;
-  }
-}
-
-//何番目のテンポかを表示する
-void setTempo(char str){
-  int rb = str - '0';
-  digitalWrite(PIN_LATCH, LOW);
-  shiftOut(PIN_SER, PIN_CLK, LSBFIRST, (int(pow(2,rb)+1)>>1));
-  digitalWrite(PIN_LATCH, HIGH);
+  delay(5);
 }
--- master/src/sketch.js
+++ master/src/sketch.js
@@ -10,6 +10,14 @@ import P5,{Image} from "p5";
     h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
   })(document);
 
+var fontFace = new FontFace(
+    'marukoi',
+    'url(./marukoi.ttf)',
+    {style: 'normal', weight: 500}
+)
+
+fontFace.load();
+
 window.onbeforeunload =  (event) => {
     player.requestPause() && player.requestStop() && player.requestMediaSeek(0);
     //player.dispose();
@@ -85,23 +93,11 @@ function onAppReady(app){
 
 //楽曲の開始可能時によびだされる
 function onTimerReady(){
-    /*if (window.name != "video"){
-        window.name="video";
-        location.reload();
-    }else{
-        window.name="";
-    }*/
     document.getElementsByClassName("textalive-media-wrapper")[0].style.display = "none";
     player.requestPause();
     artist.textContent = player.data.song.artist.name;
     song.textContent = player.data.song.name;
     document.querySelectorAll("button").forEach((btn) => (btn.disabled = false));
-    /*let p = player.video.firstPhrase;
-    jump.disabled = !p;
-    while (p && p.next) {
-        p.animate = animatePhrase;
-        p = p.next;
-    }*/
 }
 
 //拍の取得
@@ -127,18 +123,17 @@ const sketch = (p5) =>{
     const width = window.innerWidth;
     const height = window.innerHeight;
     //表示する文字数
-    const numChar = 7;
+    const numChar = 18;
     //円の半径
     let radius = height/2 - 30;
 
     let img = null;
-    const imageURL = "https://lh3.googleusercontent.com/BVUHVmvPUmjhu_gctJS_NBGZjF6ZuVIfe3bS4jzrF6gB3qsMV3tpyiE0znKn1MpOZOKaEqc-vb2NHpOmAmy2WGLlgGHEj6MVRm4_R6yu_vsihD5QMGTI0Qo65xhDIjikToPur7yBEeGyEqWw4CtB7_Qd0XwMaBpsR7Oc3Foo_N-2bGEIxjtSVu2UgbSUkjm1WqsJ5syzmODffWkkz159ZstxAm5TQQjMUoV-OPQ32b0UOt9l7nNRdinZV4pd5Ko3clMgFPGR0--zhaRu5Mlz7GpJb5D6lkPkkVqeEn7TlF7BC792AngzkF5FGG1tjufbUld7kmLFBncFsB6jelofgcRr_YogVewU23N9SMN1EXp7GHno2YLU2I4yq9kcAHN6gYHQKio63jnFV-NaVEiuIViIW27dA81IMW4ya-iNmhfdIiNZI66xTuLS6IZjXAbsYfQ6RdOIwYJUmvb7SDloOzwvPxUBQDqiRWEyMrZYdqz-cgWolbONxEnv0PbHu5_vUdnDBrtc71cVoRFsJGb8midyVACWWxc_bZvmvBt-uuxrb9mLVcT475PeCJVgq9A7Jl-IWkzQaxnx5VztFkDoxaBym2gNPcwsvs6iocLfVsCG0o78kIEu7FL1JWmKO1yE4q-crcgDI4cfB9EQZtz4ZVemCMojm7TrMFgWpngFKba6Tzs4og-x6mq_5BMj2FiQlsMI2SCYlW1X5PW86NHUCWM94hJ8kNc5BiUTDcivi_5jadGrOmjdNP1OgVcg2q1AIEW-GNafqVZJts6hvLSR6Nzkm1WD0lMwPPBLwsrMpoefIqgC8kLVBXdoyLqS0P0cBCtl-ZHcYfVQWIuS7lPOCplR3Hw7yXlmDNARtYpuXFFWYxu3GNmjtuxI31yRQqlHO-ndcMkgtdgBoOF_Fh-2ry59K5MUiCNV7B3bHkpc3QQiX2VDFDppUzG6aeHhGmYx0XM_mRWA_K9UfZL2dCtVtg=s924-no?authuser=1"
+    const imageURL = "https://raw.githubusercontent.com/kurusunagisa/kurusunagisa/main/earth.png"
     p5.preload = () => {
         img = p5.loadImage(imageURL);
     }
 
     p5.setup = () =>{
-
     p5.createCanvas(width,height);
     p5.colorMode(p5.HSB,360, 100,100,100);
     p5.frameRate(30);
@@ -169,8 +164,10 @@ const sketch = (p5) =>{
 
     let star = []
 
+    let indexRad = 0;
+
     p5.setStar = (flag) =>{
-        p5.background(0);
+        //p5.background(0);
         p5.stroke(255);
         if(flag){
             star.length = 0;
@@ -188,51 +185,52 @@ const sketch = (p5) =>{
     let earthRad = 0;
 
     p5.draw = () => {
+        p5.background(0);
         if (!player || !player.video || !player.isPlaying){
             return;
         }
 
         const position = player.timer.position;
 
+        const chorus = player.getChoruses();
+        //らせん
+        if(position < chorus[1]?.startTime){
+            const h = 200 * Math.sin(indexRad);
+            p5.push();
+            const x_1 = h + width / 2;
+            const x_2 = - h + width / 2;
+            const y = height - indexRad*10 % 400 - 450;
+            //p5.fill(0);
+            p5.fill(255);
+            p5.circle(x_1,y,10);
+            p5.circle(x_2,y,10);
+            p5.pop();
+            //console.log(x_1, x_2, y);
+            if(indexRad >= 2 * Math.PI * 5){
+                indexRad = 0;
+            }
+            indexRad+=0.1;
+        }
+
+
         let char = player.video.findChar(position+150, {loose: true});
         let bchar = player.video.findChar(position);
         let word = player.video.findWord(position+200, {loose: true});
-        let phrase = player.video.findPhrase(position+300);
-        //console.log(char.text);
-        //一定以上の時間がたった文字は消す
-        /*if(Math.round(word.endTime + 300)/100 == position/100){
-            texts.forEach((text, index) =>{
-                if(text.endTime+1000 < position){
-                    texts.splice(index,1);
-                    //delete texts[index];
-                }
-            });
-        }*/
-        //console.log(phrase?.endTime/100,position/100);
+        let phrase = player.video.findPhrase(position+150);
+
+        //console.log(phrase?.text);
+
         phraseEnd = isNaN(phrase?.endTime) ? phraseEnd : phrase?.endTime;
-        //console.log(phraseEnd);
-        if(Math.round((phraseEnd)/120) == Math.round((position)/120)){
-            p5.setStar(true);
-        }
+
         //近づいたら文字を表示
-        //console.log(player.video.findIndex(char))
         if(bchar?.endTime > position){
             p5.setStar(false);
         }
         if(char?.startTime - 150 < position){
-            //文字が上限を超えたら最初の要素から消す
-            /*if(texts.length >= numChar){
-                texts.shift();
-                p5.background(60);
-            }*/
-            //texts.push(char);
-            //console.log(texts);
-            previousIndex = index;
-            if(previousChar?.endTime != char?.endTime){
+            if(previousChar?.endTime < char?.endTime){
                 index++;
             }
-            console.log(index, player.video.findIndex(char), char?.text, char?.endTime);
-            //console.log(index);
+            //console.log(index, player.video.findIndex(char), char?.text, char?.endTime);
             previousChar = char;
 
             const vocal = player.getVocalAmplitude(position+60);
@@ -240,12 +238,11 @@ const sketch = (p5) =>{
 
             let size;
             let alpha;
+            //文字が表示される100ms前からだんだんと大きくする
             if(position < char.startTime + 100){
                 const progress = (char.startTime - position)/1000;
                 const eased = Ease.bounceOut(progress);
-                //console.log("eased:",eased);
                 size = (vocal/maxVocal)*200+0.12 * height * (1- eased);
-                //console.log("size:", size);
                 alpha = 100 - (char.startTime - position)/4;
             }
             /*if(position > char.startTime + 150){
@@ -264,8 +261,15 @@ const sketch = (p5) =>{
                 radius = height/2 - 60;
             }*/
 
-            x = width / 2 + radius * cosrad[index%numChar];
-            y = height - 80 - radius * sinrad[index%numChar];
+            //x = width / 2 + radius * cosrad[index%numChar];
+            //y = height - 80 - radius * sinrad[index%numChar];
+
+            //文字の表示
+            if(char.startTime == phrase.startTime){
+                index += numChar - index%numChar;
+            }
+            x = width/2 - (phrase?.text.length) / 2 * 50 + index%numChar * 50;
+            y = height / 2;
             p5.fill(index%1000,255,255,70,alpha);
             p5.circle(x,y,size);
             p5.fill(0,0,100);
@@ -281,9 +285,9 @@ const sketch = (p5) =>{
             //p5.rotate(Math.PI/2-rad);
             p5.text(text.text,x,y);
         });*/
+
+        //地球の回転
         const beat = player.findBeat(position);
-
-
         p5.push();
         p5.translate(width/2,height);
         if(beat){
@@ -297,6 +301,20 @@ const sketch = (p5) =>{
         p5.rotate(earthRad+=Math.PI/720);
         p5.image(img, -height/3.3, -height/3.3, height/1.65, height/1.65);
         p5.pop();
+
+        //タイトル表示
+        if(position > 17000 && position < 22000){
+            p5.push();
+            p5.fill(0,0,100);
+            p5.textSize(40);
+            p5.textFont("vdl-linegr");
+            p5.text(player.data.song.name, width/2-40,height/2-30);
+            p5.textSize(30);
+            p5.text(player.data.song.artist.name,width/2-40,height/2+30);
+            p5.pop();
+        }
+
+        
     }
 }
 
